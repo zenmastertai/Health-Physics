f0a7d059b7d99f73992cc0d7f3dd6aec1fbf9878
Tyler Cantrell <tcantrell6014@gmail.com>
2019-01-03 16:41:02 -0500

    Update LinearInterpolationGUI.py

--- LinearInterpolationGUI.py
+++ LinearInterpolationGUI.py
@@ -1,16 +1,41 @@
-#initialize lists
-data_array = []
-energy = []
-attenuation = []
+import tkinter as tk
+from tkinter import ttk
 
-#open file and extract data
-fin = open('PbMassAttenData.txt')
-for line in fin:
-    data_array.append(line)
-fin.close()
+class Root(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-#extract data to lists
-for i in range(len(data_array)):
-    e, a = list(map(str, data_array[i].split(",")))
-    energy.append(e)
-    attenuation.append(a)
+        self.title("Linear Interpolation GUI")
+        self.geometry("300x400")
+
+        options = {'Lead','Tungsten'}
+
+        self.frame = tk.Frame(self)
+        self.frame.grid()
+
+        dropdown = tk.OptionMenu(self.frame,*options)
+        dropdown.grid()
+
+
+
+    def get_atten_data(self,event=None):
+        # initialize lists
+        data_array = []
+        self.energy = []
+        self.attenuation = []
+
+        # open file and extract data
+        fin = open('PbMassAttenData.txt')
+        for line in fin:
+            data_array.append(line)
+        fin.close()
+
+        # extract data to lists
+        for i in range(len(data_array)):
+            e, a = list(map(str, data_array[i].split(",")))
+            energy.append(e)
+            attenuation.append(a)
+
+if __name__ == "__main__":
+    window = Root()
+    window.mainloop()
