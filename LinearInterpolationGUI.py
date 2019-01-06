181fa1fe455b9ca8505e52ee8e8f0ca32e24e660
Tyler Cantrell <tcantrell6014@gmail.com>
2019-01-06 16:56:46 -0500

    Update LinearInterpolationGUI.py

--- LinearInterpolationGUI.py
+++ LinearInterpolationGUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import math
+import tkinter.messagebox as msg
 
 class Root(tk.Tk):
     def __init__(self):
@@ -25,12 +26,23 @@ class Root(tk.Tk):
 
         #create label
         self.energy_input_label = tk.Label(self.frame, text="Energy (keV):")
-        self.attenuation_output_label = tk.Label(self.frame, text="u/p (cm^2/g):")
+        self.attenuation_output_label = tk.Label(self.frame, text="u (cm^-1):")
         self.shielding_mat_label = tk.Label(self.frame, text="Shielding Material:")
-        
+        self.HVL_output_label = tk.Label(self.frame, text="HVL (cm):")
+        self.TVL_output_label = tk.Label(self.frame, text="TVL (cm):")
+
+
         self.outputv = tk.StringVar()
         self.outputv.set('0')
         self.attenuation_output = tk.Label(self.frame, text=self.outputv.get())
+
+        self.outputhvl = tk.StringVar()
+        self.outputhvl.set('0')
+        self.HVL_output = tk.Label(self.frame, text=self.outputhvl.get())
+
+        self.outputtvl = tk.StringVar()
+        self.outputtvl.set('0')
+        self.TVL_output = tk.Label(self.frame, text=self.outputtvl.get())
 
         #create button widgest
         calculate_button = tk.Button(self.frame, text="Calculate")
@@ -43,10 +55,15 @@ class Root(tk.Tk):
         self.shielding_mat_label.grid(row=1,column=0,sticky='W')
         self.dropdown.grid(row=1,column=1,sticky='EW')
         self.dropdown.configure(width=8) #adjust width of the dropdown
-        calculate_button.grid(row=2,column=0,sticky='W')
+        calculate_button.grid(row=0,column=2,sticky='W')
+
         
-        self.attenuation_output_label.grid(row=0,column=2, sticky='E')
-        self.attenuation_output.grid(row=0,column=3)
+        self.attenuation_output_label.grid(row=2,column=0, sticky='E')
+        self.attenuation_output.grid(row=2,column=1)
+        self.HVL_output_label.grid(row=3,column=0,sticky='E')
+        self.HVL_output.grid(row=3,column=1)
+        self.TVL_output_label.grid(row=4,column=0,sticky='E')
+        self.TVL_output.grid(row=4,column=1)
 
         #pack input frame
         self.frame.grid()
@@ -79,27 +96,39 @@ class Root(tk.Tk):
             attenuation.append(float(a))
 
         #interpolate data to acquire attenuation coefficeint for energy input
-        energy_input = int(self.energy_input.get(1.0,tk.END))
-        for j in range(len(energy)):
-            if energy[j] == energy_input:
-                y3 = attenuation[j]
-            elif energy[j] > energy_input:
-                x3 = energy[j]
-                x2 = energy_input
-                x1 = energy[j-1]
-                y3 = attenuation[j]
-                y1 = attenuation[j-1]
-                e_coef =(((x2-x1)*(y3-y1))/(x3-x1))+y1 #Linear Interpolation
-                break
 
-        return e_coef
+        
+        if self.energy_input.get(1.0,tk.END)=="\n":
+            msg.askokcancel("Confirm", "Please enter a number")
+        else:
+            try:
+                energy_input = int(self.energy_input.get(1.0,tk.END))
+                for j in range(len(energy)):
+                    if energy[j] == energy_input:
+                        y3 = attenuation[j]
+                    elif energy[j] > energy_input:
+                        x3 = energy[j]
+                        x2 = energy_input
+                        x1 = energy[j-1]
+                        y3 = attenuation[j]
+                        y1 = attenuation[j-1]
+                        e_coef =(((x2-x1)*(y3-y1))/(x3-x1))+y1 #Linear Interpolation
+                        break
+
+                return e_coef
+            except ValueError:
+                msg.askokcancel("Confirm", "Please enter a number")
 
     def calculate(self,event=None):
-        e_coef = round(self.interpolate(),4)
-        HVL = math.log(1/2)/-(e_coef*self.density[self.v.get()])
-        TVL = math.log(1/10)/-(e_coef*self.density[self.v.get()])
-        print(HVL)
-        self.attenuation_output['text']=str(e_coef)  
+        try:
+            e_coef = round(self.interpolate()*self.density[self.v.get()],2)
+            HVL = round(math.log(1/2)/-e_coef,4)
+            TVL = round(math.log(1/10)/-e_coef,4)
+            self.attenuation_output['text']=str(e_coef)
+            self.HVL_output['text']=str(HVL)
+            self.TVL_output['text']=str(TVL)
+        except TypeError:
+            pass
         
 if __name__ == "__main__":
     window = Root()
