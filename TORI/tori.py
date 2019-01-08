8df5bf5e9817c670a75b85daead93801491bd1cc
Cantrell <tyler.cantrell@cardinalhealth.com>
2019-01-08 09:10:55 -0500

    updates

--- TORI/tori.py
+++ TORI/tori.py
@@ -1,3 +1,19 @@
 from isoref import *
 
-print(nuclides[1])
+def import_data(filename):
+    data = []
+    file = open(filename)
+    for line in file:
+        tmp_data = []
+        line = line.split(',')
+        tmp_data.append(line[0])
+        tmp_data.append(line[2])
+        tmp_data.append(line[4])
+        tmp_data.append(line[6].strip('\n'))
+        data.append(tmp_data)
+    file.close()
+    return data
+
+gamma_data = import_data('gammas.txt')
+print(gamma_data[0])
+
