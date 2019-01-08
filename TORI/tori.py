25a377c7848a2490e50cd9da8cd67685433834b7
Tyler Cantrell <tcantrell6014@gmail.com>
2019-01-08 17:11:08 -0500

    changes

--- TORI/tori.py
+++ TORI/tori.py
@@ -29,33 +29,46 @@ for i in range(len(isotope_data["data"])):
     gdb[isotope_data["data"][i]["ref"]]=[{}]
     gamma_db["data"].append(gdb)
 
-print(len(gamma_db["data"]))
+##for i in range(1000):
+##    print(gamma_db["data"][i])
+##    
+##    if '30011' in gamma_db["data"][i]:
+##        print('THE ROW IS ' + str(i))
 
-##gamma_db['30011'][0]['gamma1']=123
-##print(gamma_db['30011'][0]['gamma1'])
+#gamma_db["data"][0]['120031'][0]['gamma1'] = 123
+#gamma_db["data"][0]['120031'][0]['gamma2'] = 124
+##print(len(gamma_db["data"]))
+print(gamma_db["data"][0])
+
+##if 'gamma1' in gamma_db["data"][0]['120031'][0]:
+##    print("HERE")
 
 gamma_data = import_radiation_data('gammas.txt')
 
 testdata = gamma_data[0:20]
 testdata.sort(key=lambda gamma: gamma[1], reverse=False)
 
+##for line in testdata:
+##    print(line)
+
 for i in range(len(testdata)):
     count = 1
-    #print(testdata[i][0])
-    #print(gamma_db['30011'][0])
-    if 'gamma'+str(count) in gamma_db[testdata[i][0]][0]:
-        count+=1
-    else:
-        gamma_db[testdata[i]][0]['gamma'+str(count)]=testdata[i][1]
+    for j in range(len(gamma_db["data"])):
+        try:
+            if 'gamma'+str(count) in gamma_db["data"][j][testdata[i][0]][0]:
+                count+=1
+            else:
+                gamma_db["data"][j][testdata[i][0]][0]['gamma'+str(count)] = testdata[i][1]
+        except KeyError:
+            pass
+
+print(gamma_db["data"])
+#the problem is that i need a nested loop here to loop through both gamma_db and testdata
 
 #print(gamma_db['120031'])
 
 
 ##print(gamma_db['120031'][0])
 
-##for i in range(len(testdata)):
-##    print(testdata[i])
-    
 
-#print(isotope_data['data'][0])
 
